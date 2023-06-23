0ad12a38f3ec18cf66c465ac87edff4d4e845896
umamahesh.gudiboina <umamahesh.gudiboina@DESKTOP-5LJD80V.Tradeleaves.com>
2023-06-23 17:22:13 +0530

    added hello world class for the git checkup

---
+++ src/main/java/com/cglia/user/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.cglia.user;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("this is a hello world class for checking git commit from eclipse after modifying the code");
+	}
+}

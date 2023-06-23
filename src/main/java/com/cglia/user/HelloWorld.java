843c7767cbc172a0898bcaaaf4d45d693a644c22
umamahesh2936 <135868159+umamahesh2936@users.noreply.github.com>
2023-06-23 17:27:59 +0530

    changed data in git and pulling to eclipse that changes

--- src/main/java/com/cglia/user/HelloWorld.java
+++ src/main/java/com/cglia/user/HelloWorld.java
@@ -2,6 +2,6 @@ package com.cglia.user;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("this is a hello world class for checking git commit from eclipse after modifying the code");
+		System.out.println("this is a hello world class for checking git commit from eclipse after modifying the code.........");
 	}
 }
